d02cb4989e0045abf5b9b0ea6f8a35343438c61f
Daniel Bergey <bergey@alum.mit.edu>
2014-07-19 21:14:03 +0000

    improve Haskell environments
    factor out duplicated code,
    improve naming
    
    This is still for a cabal-install / cabal-sandbox centric workflow

--- config.nix
+++ config.nix
@@ -1,52 +1,35 @@
 pkgs : 
 { 
-  cabal.libraryProfiling = true;
-  packageOverrides = self : rec {
-    ghc74 = self.haskellPackages_ghc742.ghcWithPackagesOld (hsPkgs : [
-    ]);
-    ghc74-platform = self.haskellPackages_ghc742.ghcWithPackagesOld (hsPkgs : [
-      hsPkgs.haskellPlatform
-    ]);
-    ghc76 = self.haskellPackages_ghc763.ghcWithPackagesOld (hsPkgs : []);
-    # everything I like to have around to make Diagrams builds faster
-    ghc76-diagrams = self.haskellPackages_ghc763.ghcWithPackagesOld (hsPkgs : [
-      hsPkgs.zlib
-      hsPkgs.OpenGL
-      hsPkgs.GLFWB
-      hsPkgs.glib
-      hsPkgs.pango
-      hsPkgs.lens
-    ]);
-    ghc76-cairo = self.haskellPackages_ghc763.ghcWithPackagesOld (hsPkgs : [ 
-    hsPkgs.zlib
-    hsPkgs.pango
-    hsPkgs.text-icu
-    ]);
-    ghc76-zlib = self.haskellPackages_ghc763.ghcWithPackagesOld (hsPkgs : [
+  allowUnfree = true;
+  packageOverrides = self: with self; rec {
+    # common haskell packages with external dependencies
+    external-deps = (hsPkgs: [
       hsPkgs.zlib
       hsPkgs.terminfo
     ]);
-    ghc78 = self.haskellPackages_ghc782.ghcWithPackagesOld (hsPkgs : [ ]);
-    ghc78-diagrams = self.haskellPackages_ghc782.ghcWithPackagesOld (hsPkgs : [ 
+    # as much as is useful for building Diagrams quickly
+    diagrams-deps = (hsPkgs: [
       hsPkgs.zlib
+      hsPkgs.terminfo
       hsPkgs.OpenGL
       hsPkgs.GLFWB
       hsPkgs.glib
-      hsPkgs.pango
-      hsPkgs.lens
       hsPkgs.gio
       hsPkgs.gtk
+      hsPkgs.pango
+      hsPkgs.lens
+      hsPkgs.textIcu
     ]);
-    ghc78-cairo = self.haskellPackages_ghc782.ghcWithPackagesOld (hsPkgs : [ 
-    hsPkgs.zlib
-    hsPkgs.pango
-    ]);
-    ghc78-base = self.haskellPackages_ghc782.ghcWithPackagesOld (hsPkgs : [
-    hsPkgs.zlib
-    hsPkgs.terminfo]);
-    ghcHEAD = self.haskellPackages_ghcHEAD.ghcWithPackagesOld (hsPkgs : [
-    hsPkgs.zlib
-    ]);
+    ghc74-bare = self.haskellPackages_ghc742.ghcWithPackagesOld (hsPkgs : [ ]);
+    ghc74 = self.haskellPackages_ghc742.ghcWithPackagesOld external-deps;
+    ghc74-diagrams = self.haskellPackages_ghc742.ghcWithPackagesOld diagrams-deps;
+    ghc76-bare = self.haskellPackages_ghc763.ghcWithPackagesOld (hsPkgs : []);
+    ghc76 = self.haskellPackages_ghc763.ghcWithPackagesOld external-deps;
+    ghc76-diagrams = self.haskellPackages_ghc763.ghcWithPackagesOld diagrams-deps;
+    ghc78-bare = self.haskellPackages_ghc782.ghcWithPackagesOld (hsPkgs : [ ]);
+    ghc78 = self.haskellPackages_ghc782.ghcWithPackagesOld external-deps;
+    ghc78-diagrams = self.haskellPackages_ghc782.ghcWithPackagesOld diagrams-deps;
+    ghcHEAD = self.haskellPackages_ghcHEAD.ghcWithPackagesOld external-deps;
 
     pythonEnv = self.myEnvFun {
       name = "python";
