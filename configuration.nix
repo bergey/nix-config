50b7d7632c6582e9227a03fcf3755a70c756b9ec
Daniel Bergey <bergey@teallabs.org>
2018-10-14 21:04:49 +0000

    u2f option is no longer recognized

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,6 @@ virtualisation.docker.enable = true;
   programs.ssh.startAgent = false;
 
   hardware.pulseaudio.enable = true;
-  hardware.u2f.enable = true
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
