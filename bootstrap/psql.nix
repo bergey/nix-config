2d7707645fa28a1764249c1e6039fb6b0eb9cc48
Daniel Bergey <bergey@teallabs.org>
2018-12-16 20:55:54 +0000

    bootstrap for Postgres `psql`

---
+++ bootstrap/psql.nix
@@ -0,0 +1,17 @@
+let
+  fetchNixpkgs = import ../fetchNixpkgs.nix;
+
+nixpkgs = fetchNixpkgs (builtins.fromJSON (builtins.readFile ../nixpkgs-snapshot.json));
+
+  pkgs = import nixpkgs { config = {}; };
+
+  mkEnv = env: if pkgs.lib.inNixShell
+        then pkgs.mkShell (env // {buildInputs = env.paths;})
+        else pkgs.buildEnv env;
+
+in with pkgs; mkEnv {
+        name = "bootstrap-psql";
+    paths = [
+        postgresql
+        ];
+    }
