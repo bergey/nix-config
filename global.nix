c0b491227506a334b1e8e7dd679b302c70ce934a
Daniel Bergey <bergey@teallabs.org>
2019-04-01 16:38:19 -0400

    add `sizes` from github
    I use `sizes` with git-annex, and the Hackage version is been broken
    since semigroup-monoid

--- global.nix
+++ global.nix
@@ -8,6 +8,31 @@ nixpkgs = fetchNixpkgs (builtins.fromJSON (builtins.readFile ./nixpkgs-snapshot.
         };
     };
 
+    sizes =
+        ({ mkDerivation, base, bytestring, cmdargs, deepseq, dlist, lens
+        , parallel-io, regex-posix, system-fileio, system-filepath, text
+        , unix
+        }:
+        mkDerivation {
+            pname = "sizes";
+            version = "2.3.2-git";
+            src = pkgs.fetchgit {
+                url = "https://github.com/jwiegley/sizes.git";
+                sha256 = "0ma8kxw1sh3bg2rqgq7i6pbjg5frjvyyjshma8q98r8sqa579yn2";
+                rev = "1658de1c70d505f2e5d9d736975a8bfae77799f1";
+            };
+            isLibrary = false;
+            isExecutable = true;
+            executableHaskellDepends = [
+                base bytestring cmdargs deepseq dlist lens parallel-io regex-posix
+                system-fileio system-filepath text unix
+            ];
+            description = "Recursively show space (size and i-nodes) used in subdirectories";
+            license = pkgs.stdenv.lib.licenses.bsd3;
+            hydraPlatforms = pkgs.stdenv.lib.platforms.none;
+            broken = false;
+            });
+
 in with pkgs;
 
 buildEnv {
@@ -30,7 +55,7 @@ buildEnv {
     haskellPackages.hlint
     haskellPackages.graphmod
     # haskellPackages.HaRe
-    # haskell.packages.ghc822.sizes # broken since semigroup-monoid
+    (haskellPackages.callPackage sizes {})
     htop
     id3v2
     inkscape
