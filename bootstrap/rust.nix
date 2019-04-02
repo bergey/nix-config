6759b9da353a549b959a4185089b89e264723c3e
Daniel Bergey <bergey@teallabs.org>
2019-04-02 14:21:26 -0400

    add bootstrap rust

---
+++ bootstrap/rust.nix
@@ -0,0 +1,16 @@
+let
+    fetchNixpkgs = import ../fetchNixpkgs.nix;
+    nixpkgs = fetchNixpkgs (builtins.fromJSON (builtins.readFile ../nixpkgs-snapshot.json));
+    pkgs = import nixpkgs { config = {}; };
+
+    mkEnv = env: if pkgs.lib.inNixShell
+          then pkgs.mkShell (env // {buildInputs = env.paths;})
+          else pkgs.buildEnv env;
+
+in with pkgs; mkEnv {
+        name = "bootstrap-rust";
+        paths = [
+            cargo
+            rustc
+        ];
+    }
