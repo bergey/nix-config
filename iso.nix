cdf72bc74e547ebc8a4df039b0ed0d80138d18d7
Daniel Bergey <bergey@teallabs.org>
2019-08-14 20:19:55 +0000

    iso: add nmtui and other tools useful in bootstrapping

--- iso.nix
+++ iso.nix
@@ -3,11 +3,38 @@
 # contain any graphical stuff.
 {config, pkgs, ...}:
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    imports = [
+        <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
 
-    # Provide an initial copy of the NixOS channel so that the user
-    # doesn't need to run "nix-channel --update" first.
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-  ];
+        # Provide an initial copy of the NixOS channel so that the user
+        # doesn't need to run "nix-channel --update" first.
+        <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    ];
+
+    networking.wireless.enable = false;
+    networking.networkmanager.enable = true; # NetworkManager, including nmcli & nmtui (no applet)
+
+    i18n = {
+        consoleKeyMap = "dvorak";
+        defaultLocale = "en_US.UTF-8";
+    };
+    time.timeZone = "UTC";
+  
+    environment.systemPackages = with pkgs; [
+        acpi
+        curl
+        git
+        gitAndTools.hub
+        gnupg
+        mr
+        pass
+        ripgrep
+        rsync
+        stow
+        w3m
+        wget
+        vim
+    ] ++ (import <nixpkgs/nixos/modules/profiles/base.nix> {lib = pkgs.lib; inherit pkgs;}).environment.systemPackages;
+
+    hardware.u2f.enable = true;
 }
