bc1343c4f940b85a35cd04d57bb6f70ff0e18200
Daniel Bergey <bergey@teallabs.org>
2019-08-14 18:37:08 +0000

    add exFAT driver

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
 
   networking.hostName = "prandtl"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
