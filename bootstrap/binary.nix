6f74f7ac0e0c8db589804d95911fe1e75c3bb4d0
Daniel Bergey <bergey@teallabs.org>
2019-09-03 21:10:44 +0000

    new binary bootstrap for debugging tools I've used recently

---
+++ bootstrap/binary.nix
@@ -0,0 +1,27 @@
+let
+    nixpkgs =
+        let snapshot = builtins.fromJSON (builtins.readFile ../nixpkgs-snapshot.json);
+        inherit (snapshot) owner repo rev;
+        in builtins.fetchTarball {
+            inherit (snapshot) sha256;
+            url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+            };
+    pkgs = import nixpkgs { config = {}; };
+
+    mkEnv = env: if pkgs.lib.inNixShell
+          then pkgs.mkShell (env // {buildInputs = env.paths;})
+          else pkgs.buildEnv env;
+
+in with pkgs; mkEnv {
+  name = "bootstrap-binary";
+  paths = [
+    bind
+    binutils
+    glxinfo
+    intel-gpu-tools
+    opencl-info
+    pciutils
+    smem
+  ];
+}
+    
